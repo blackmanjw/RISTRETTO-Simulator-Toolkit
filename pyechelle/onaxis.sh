58de2f4c3410a2401c90d46897bc1e6aad70b4f8
Joshua Blackman <joshua.blackman@utas.edu.au>
2025-10-20 16:22:22 +0200

    Update onaxis.sh

--- pyechelle/onaxis.sh
+++ pyechelle/onaxis.sh
@@ -3,6 +3,7 @@
 #SBATCH --error logs/log.e%j 
 #SBATCH --output logs/log.o%j  
 #SBATCH --gres=gpu:rtx3090:1
+#SBATCH --mem-per-gpu=8G
 #SBATCH --partition=gpu
 #SBATCH --qos=job_gpu
 #SBATCH --time 0-00:25:00 
