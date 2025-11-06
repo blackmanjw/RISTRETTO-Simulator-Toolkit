395cabc895e4cc650c1fa631209a5a3ff34f2881
Joshua Blackman <joshua.blackman@utas.edu.au>
2025-11-06 13:59:26 +0100

    Update simulation_lovis.py

--- simulation_lovis.py
+++ simulation_lovis.py
@@ -4,6 +4,7 @@ import numpy as np
 import astropy.io.fits as fits
 import matplotlib.pyplot as plt
 import smplotlib
+import os
 
 filelist=glob.glob('harps_pds70/*.fits')
 filelist.sort()
@@ -39,10 +40,13 @@ master=master/master[(wave>6583.)&(wave<6585.)].mean()*flux_density
 waveHa,fluxHa=np.loadtxt('Halpha_model/Ha_60_14.dat',unpack=True)
 waveHa=waveHa[::-1]
 fluxHa=fluxHa[::-1]
+fluxorig=fluxHa
 # Planet radius = 2 R_J, emitting surface = 1% of planet surface
 fluxHa=fluxHa*(2.*6.9911e7)**2*0.01/(113.4*3.0857e16)**2
 # Normalization to get a line flux of 8.1e-16 erg/s/cm2 as for PDS 70b in Hashimoto et al. (2020)
-fluxHa=fluxHa/fluxHa.sum()/0.05123*8.1e-16
+dl = np.mean(np.diff(waveHa))
+fluxHa = fluxHa / fluxHa.sum() / dl * 8.1e-16
+#fluxHa=fluxHa/fluxHa.sum()/0.05123*8.1e-16
 model=np.interp(wave,waveHa,fluxHa,left=0.,right=0.)
 
 # SNR reference: SNR=45 per pixel of 0.82 km/s in 1800s for HARPS
@@ -88,4 +92,41 @@ plt.ylabel('Flux (e-)')
 plt.title('VLT-RISTRETTO Simulation of a Planet at 3-5 AU around PDS 70 (Texp=1 hour)')
 plt.show()
 
+plt.figure(4,figsize=(12.,7.))
+plt.clf()
+plt.plot(waveHa,FluxHa,'tab:blue')
+plt.plot(waveHa,Fluxorig,'tab:blue')
+plt.yscale('log')
+#plt.xlim(-240.,90.)
+#plt.ylim(-25.,85.)
+plt.xlabel('Radial Velocity (km/s)')
+plt.ylabel('Flux (e-)')
+plt.title('VLT-RISTRETTO Simulation of a Planet at 3-5 AU around PDS 70 (Texp=1 hour)')
+plt.show()
+
+# Create output directory if it doesn't exist
+outdir = "simulation_lovis"
+os.makedirs(outdir, exist_ok=True)
+
+# --- Save wave, master, and master+model ---
+csv1 = os.path.join(outdir, "simul_lovis.csv")
+np.savetxt(
+    csv1,
+    np.column_stack((wave, master, master + model)),
+    delimiter=",",
+    header="wavelength_A,master_flux,master_plus_model_flux",
+    comments="",
+)
+print(f"Saved {csv1}")
+
+# --- Save rv and simulated counts ---
+csv2 = os.path.join(outdir, "simul_lovis_3-5au.csv")
+np.savetxt(
+    csv2,
+    np.column_stack((rv, simul)),
+    delimiter=",",
+    header="rv_kms,simulated_flux_electrons",
+    comments="",
+)
+print(f"Saved {csv2}")
 
