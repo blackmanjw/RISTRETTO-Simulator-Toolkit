1a89d6697d90bd4a6702505c739c6540d772698e
Joshua Blackman <joshua.blackman@utas.edu.au>
2025-11-14 16:01:19 +0100

    Update runall.sh

--- runall.sh
+++ runall.sh
@@ -9,3 +9,4 @@ python resample.py PDS70b_BT-Settl-CIFIST-1400K-4logg.txt --R 140000
 python resample.py PDS70_star_BT-Settl-CIFIST-4200K-5logg.txt --R 140000
 python resample.py 2MJ1612b_BT-Settl-CIFIST-1200K-3.5logg.txt --R 140000
 python resample.py 2MJ1612_star_BT-Settl-CIFIST-3900K-4logg.txt --R 140000
+python harps_pds70.py
