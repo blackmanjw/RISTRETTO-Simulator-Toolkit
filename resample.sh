ea0f65a11e3b53890c77105f11749194bb422bac
Joshua Blackman <joshua.blackman@utas.edu.au>
2025-11-17 16:37:39 +0100

    Update resample.sh

--- resample.sh
+++ resample.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
 
-python resample.py PDS70c_BT-Settl-CIFIST-1300K-4logg.txt --R 140000
-python resample.py WISPIT2_star_BT-Settl-CIFIST-4400K-4logg.txt --R 140000
-python resample.py WISPIT2b_BT-Settl-CIFIST-1400K-4logg.txt --R 140000
-python resample.py PDS70b_BT-Settl-CIFIST-1400K-4logg.txt --R 140000
-python resample.py PDS70_star_BT-Settl-CIFIST-4200K-5logg.txt --R 140000
-python resample.py 2MJ1612b_BT-Settl-CIFIST-1200K-3.5logg.txt --R 140000
-python resample.py 2MJ1612_star_BT-Settl-CIFIST-3900K-4logg.txt --R 140000
+python resample.py PDS70c_BT-Settl-CIFIST-1300K-4logg.txt --R 140000 --no-show
+python resample.py WISPIT2_star_BT-Settl-CIFIST-4400K-4logg.txt --R 140000 --no-show
+python resample.py WISPIT2b_BT-Settl-CIFIST-1400K-4logg.txt --R 140000 --no-show
+python resample.py PDS70b_BT-Settl-CIFIST-1400K-4logg.txt --R 140000 --no-show
+python resample.py PDS70_star_BT-Settl-CIFIST-4200K-5logg.txt --R 140000 --no-show
+python resample.py 2MJ1612b_BT-Settl-CIFIST-1200K-3.5logg.txt --R 140000 --no-show
+python resample.py 2MJ1612_star_BT-Settl-CIFIST-3900K-4logg.txt --R 140000 --no-show
